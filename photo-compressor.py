fbfb8b10cf930eed6500a099da14f40df1f12fb4
Sebian <256605829+Sebian12@users.noreply.github.com>
2026-05-23 15:33:55 +0200

    Added listbox to show photos separately

--- photo-compressor.py
+++ photo-compressor.py
@@ -1,15 +1,20 @@
+import os
 from tkinter import *
 from tkinter import filedialog
 from PIL import Image
 
 def select_photos ():
-    filename = filedialog.askopenfilenames(initialdir="/", title="Select file", filetypes=(("Photos", "*.png *.jpg *.jpeg"), ("All Files", "*.*")))
-    print(filename)
-
+    filename = filedialog.askopenfilenames(initialdir="/", title="Select file",filetypes=(("Photos", "*.png *.jpg *.jpeg"), ("All Files", "*.*")))
+    if filename:
+        for file in filename:
+            listbox.insert(END, os.path.basename(file))
 
 app = Tk()
 app.title("Photo Compressor")
 app.geometry("500x500")
 Button(app, text="Select Photos", command=select_photos).pack()
+
+listbox = Listbox(app)
+listbox.pack()
 
 app.mainloop()
