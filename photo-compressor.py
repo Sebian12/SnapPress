ffdac397df78b464271ced5b51081778c3f56493
Sebian <256605829+Sebian12@users.noreply.github.com>
2026-05-23 15:13:46 +0200

    Added function to add photos with a button

--- photo-compressor.py
+++ photo-compressor.py
@@ -1,9 +1,15 @@
-import tkinter as tk
+from tkinter import *
 from tkinter import filedialog
 from PIL import Image
 
-app = tk.Tk()
+def select_photos ():
+    filename = filedialog.askopenfilenames(initialdir="/", title="Select file", filetypes=(("Photos", "*.png *.jpg *.jpeg"), ("All Files", "*.*")))
+    print(filename)
+
+
+app = Tk()
 app.title("Photo Compressor")
 app.geometry("500x500")
+Button(app, text="Select Photos", command=select_photos).pack()
 
 app.mainloop()
